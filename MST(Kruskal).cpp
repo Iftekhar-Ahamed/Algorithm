fd4094af2834336ea926192f08d7558088dd6016
IFTEKHAR AHAMED SIDDIQUEE <70750068+Iftekhar-Ahamed@users.noreply.github.com>
2022-12-27 10:07:56 +0600

    Update MST(Kruskal).cpp

--- MST(Kruskal).cpp
+++ MST(Kruskal).cpp
@@ -31,78 +31,91 @@
 #include <assert.h>
 using namespace std;
 
-#define read freopen("0_input.txt","r",stdin)
-#define write freopen("0_output.txt","w",stdout)
+#define read freopen("0_input.txt", "r", stdin)
+#define write freopen("0_output.txt", "w", stdout)
 typedef long int int32;
 typedef unsigned long int uint32;
 typedef long long int int64;
 typedef unsigned long long int uint64;
-#define lld long long
-#define INF (lld)1e16
+#define ll long long
+#define INF (ll)1e16
 #define EPS 1e-9
 #define PI 3.1415926535897932384626433832795
 #define mXs 1e6
-#define test long long int ct;cin >> ct;while (ct--)
+#define test          \
+    long long int ct; \
+    cin >> ct;        \
+    while (ct--)
 const double pi = acos(-1.0);
-const lld modulo=1e9+7;
+const ll modulo = 1e9 + 7;
 int dRow[] = {-1, 0, 1, 0, 1, 1, -1, -1};
 int dCol[] = {0, 1, 0, -1, 1, -1, -1, 1};
 #define nn "\n"
 
-lld find(lld node,vector<lld>&p){
-    if(p[node]==-1)return node;
-    p[node]=find(p[node],p);
+ll find(ll node, vector<ll> &p)
+{
+    if (p[node] == -1)
+        return node;
+    p[node] = find(p[node], p);
     return p[node];
 }
-bool merge(lld x,lld y,vector<lld>&p){
-    x = find(x,p);
-    y = find(y,p);
-    if(x==y)return false;
-    p[x]=y;
+bool merge(ll x, ll y, vector<ll> &p)
+{
+    x = find(x, p);
+    y = find(y, p);
+    if (x == y)
+        return false;
+    p[x] = y;
     return true;
 }
 
-void solve(){
-    lld n,m,x,y,c;
-    cin>>n>>m;
-    vector<lld>p(n+1,-1);
-    vector<tuple<lld,lld,lld>>v;
-    for(lld i=0;i<m;i++){
-        cin>>x>>y>>c;
-        v.push_back({c,x,y});
+void solve()
+{
+    ll n, m, x, y, c;
+    cin >> n >> m;
+    vector<ll> p(n + 1, -1);
+    vector<tuple<ll, ll, ll>> v;
+    for (ll i = 0; i < m; i++)
+    {
+        cin >> x >> y >> c;
+        v.push_back({c, x, y});
     }
-    sort(v.begin(),v.end());
-    lld countEdge=0,cost=0;
-    for(auto [c,x,y]:v){
-        if(merge(x,y,p)){
-            cost+=c;
+    sort(v.begin(), v.end());
+    ll countEdge = 0, cost = 0;
+    for (auto [c, x, y] : v)
+    {
+        if (merge(x, y, p))
+        {
+            cost += c;
             countEdge++;
         }
     }
-    if(countEdge!=n-1){
-        cout<<"IMPOSSIBLE"<<nn;
-    }else{
-        cout<<cost<<nn;
+    if (countEdge != n - 1)
+    {
+        cout << "IMPOSSIBLE" << nn;
+    }
+    else
+    {
+        cout << cost << nn;
     }
 }
-
 
 int main()
 {
     cin.tie(NULL);
-    //read;
-    //write;
+    // read;
+    // write;
     ios_base::sync_with_stdio(false);
-    //test{
-      solve();
+    // test{
+    solve();
     //}
     return 0;
 }
 /*
-* do something instead of nothing and stay organized
-* WRITE STUFF DOWN
-* DON'T GET STUCK ON ONE APPROACH
-*/
+ * do something instead of nothing and stay organized
+ * WRITE STUFF DOWN
+ * DON'T GET STUCK ON ONE APPROACH
+ */
 /* Final check before submit :
 1. array size or integer overflow,like uninitialised 0 index.
 2. Think twice,code once.check all possible counter test case.
